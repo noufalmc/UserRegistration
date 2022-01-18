d24655df7ca9f1d01a1896378d3773580c4f503e
noufalmc123 <47408615+noufalmc123@users.noreply.github.com>
2022-01-18 19:24:00 +0530

    Added:last name validation

--- src/corejava/userRegistration.java
+++ src/corejava/userRegistration.java
@@ -7,25 +7,51 @@ User Registration
 package corejava;
 import java.util.Scanner;
 
+import static java.lang.System.exit;
+
 public class userRegistration {
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
         int choice;
-        String name;
-        System.out.println("Enter Your Choice" +
-                "[1] Name Validation");
-        choice=sc.nextInt();
+        String name,type="";
         regularExpression re=new regularExpression();
+        System.out.println("Enter Your Choice\n" +
+                "[1] Fisrt Name\n" +
+                "[2] Last Name\n" +
+                "");
+        choice=sc.nextInt();
+        if(choice==1)
+        {
+            type="First";
+        }
+        else if(choice==2)
+        {
+            type="Last";
+        }
         switch(choice)
         {
             case 1:
+            case 2:
                 sc.nextLine();
-                System.out.println("Enter First Name");
+                System.out.println("Enter "+type+" Name");
                 name=sc.nextLine();
-                System.out.println("Result is"+re.isValidName(name));
-
+                displayResult(re.isValidName(name));
+                break;
+            default:
+                exit(0);
         }
 
+    }
+    public static void displayResult(boolean val)
+    {
+        if(val)
+        {
+            System.out.println("Valid name");
+        }
+        else
+        {
+            System.out.println("Inavlid name");
+        }
     }
 }
