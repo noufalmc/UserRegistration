710e4a1923e1e9a4e8fca964db4614641ba1fa4e
noufalmc123 <47408615+noufalmc123@users.noreply.github.com>
2022-01-23 07:47:22 +0530

    Added:Test Cases

--- src/corejava/regularExpression.java
+++ src/corejava/regularExpression.java
@@ -1,8 +1,25 @@
+/*
+Author: Noufal mc
+Date:17/01/22
+ Regex validate to User Registration
+* */
+
 package corejava;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class regularExpression {
+    /*
+    * Check the firstname valid or invalid
+    * All methods returns true
+    * Regex false return false
+    * Regex true it will return true
+
+
+    *check whether first letter of name Caps and atleast 3 lengt
+    @param
+    @return
+    * * */
     public boolean isValidName(String name)
     {
         String pattern="^[A-Z][a-z]{2,}$";
@@ -10,6 +27,11 @@ public class regularExpression {
         Matcher m=p.matcher(name);
         return m.matches();
     }
+    /*
+    * Check whether user email valid or not
+     * @param email
+     * @return
+     */
     public boolean isValidEmail(String email)
     {
         String pattern="^[a-zA-Z0-9_+-]+([.][a-zA-Z0-9+]+)*@[a-zA-Z0-9+]+([.][a-zA-z]*)+[a-z]$";
@@ -17,6 +39,12 @@ public class regularExpression {
         Matcher m=p.matcher(email);
         return m.matches();
     }
+    /*
+    * check the mobile number valid or not
+    * must enter country code and follow by space 10 digit number
+    *@param mobile
+    * @return
+    * */
     public boolean isValidMobile(String mobile)
     {
         String pattern="^[0-9]{2}\\s[0-9]{10}$";
@@ -24,9 +52,15 @@ public class regularExpression {
         Matcher m=p.matcher(mobile);
         return m.matches();
     }
+    /*
+    * Validate Password
+    * Should have at least 1 uppercase
+    * Should have at numeric  number in password
+    * Should have exactly one charcter
+    * */
     public boolean isValidPassword(String mobile)
     {
-        String pattern="(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]*[@#$%_!^&*][0-9a-zA-Z]*";
+        String pattern="(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]*[@#$%_!^&*]?[0-9a-zA-Z]*$";
         Pattern p=Pattern.compile(pattern);
         Matcher m=p.matcher(mobile);
         return m.matches();
--- src/test/userRegistrationTest.java
+++ src/test/userRegistrationTest.java
@@ -1,11 +1,52 @@
 package test;
 import corejava.regularExpression;
-import org.junit.Assertions;
+import static org.junit.Assert.*;
 import org.junit.Test;
-import userregistration.UserRegistration;
 
 public class userRegistrationTest {
+    //decleration
     public static final regularExpression rs=new regularExpression();
+    @Test
+    /**
+     *Test First Name
+     *asserts Value
+     */
+    public void isValidFirstName()
+    {
+        assertTrue(rs.isValidName("Nouf"));
+        assertTrue(rs.isValidName("nouf"));
+        assertFalse(rs.isValidName("nouf"));
+    }
+    /**
+     *Test  Email
+     *asserts Value
+     */
+    @Test
+    public void isValidEmail()
+    {
+        assertTrue(rs.isValidEmail("Nouf@gmail.com"));
+        assertTrue(rs.isValidEmail("nouf"));
+        assertFalse(rs.isValidEmail("nouf"));
+    }
+    /**
+     * Test Is valid mobile
+     */
+    @Test
+    public  void  isValidMobile()
+    {
+        assertTrue(rs.isValidMobile("91 9633405658"));
+        assertFalse(rs.isValidEmail("909090"));
+        assertTrue(rs.isValidEmail("909090"));
+    }
+    /**
+     * Check the password
+     * */
+    @Test
+    public  void isValidPassword()
+    {
+        assertTrue(rs.isValidPassword("@Nhasdhahsd123"));
+        assertTrue(rs.isValidPassword("hasdhahsd1"));
 
+    }
 
 }
